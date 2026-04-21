14b89dcae8e4221614a9bfef2b6a386676efef1f
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-21 19:20:12 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/DateTime to DateTime/datetime-to-datetime.py
+++ 260421/DateTime to DateTime/datetime-to-datetime.py
@@ -1,23 +1,23 @@
+def check(a, b, c):
+    x = (a * 10000) + (b * 100) + c
+    if x >= 111111:
+        return True
+    else:
+        return False
+
 a, b, c = map(int, input().split())
 
 z = a - 11
 s = b - 11
 d = c - 11
 
-def check(a, b, c):
-    x = a * 10000 + b * 100 + c
-    if x >= 111111:
-        return True
-    else:
-        return False
-
 if check(a, b, c):
     if s < 0 :
         z -= 1
-        s += 23
+        s += 24
     if d < 0 :
         s -= 1
-        d += 59
+        d += 60
     answer = z*24*60 + s*60 + d
     print(answer)
 else:
