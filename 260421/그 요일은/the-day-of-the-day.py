fbab92a168f8b93242fda0126bf2e0a4a228961e
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-21 22:49:42 +0900

    Title: 그 요일은, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/그 요일은/the-day-of-the-day.py
+++ 260421/그 요일은/the-day-of-the-day.py
@@ -1,16 +1,17 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
-
+# print(m1, d1, m2, d2)
 arr = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-t = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+t = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 def sum_d(m, d):
-    return sum(arr[:m]) + d
-a = sum_d(m1, d1)
-b = sum_d(m2, d2)
-diff = b - a
+    return sum(arr[:m],d)
+one = sum_d(m1, d1)
+two = sum_d(m2, d2)
+# print(one, two)
+diff = two - one
 # print(diff)
-answer = (diff + 1)//7
-if (diff + 1)%7 == t.index(A):
+answer = diff//7
+if diff%7 >= t.index(A):
     answer += 1
 print(answer)
