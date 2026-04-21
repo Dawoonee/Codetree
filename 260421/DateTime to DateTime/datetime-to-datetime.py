5a82947ec0fd855fefb7eb626caf36e0a7e4edaf
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-21 18:24:25 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260421/DateTime to DateTime/datetime-to-datetime.py
+++ 260421/DateTime to DateTime/datetime-to-datetime.py
@@ -3,18 +3,22 @@ a, b, c = map(int, input().split())
 z = a - 11
 s = b - 11
 d = c - 11
-if z <= 0:
-    if s <= 0:
-        if d <= 0:
-            if z == 0 and s == 0 and d == 0:
-                print(0)
-            else:
-                print(-1)
+# if z <= 0:
+#     if s <= 0:
+#         if d <= 0:
+#             if z == 0 and s == 0 and d == 0:
+#                 print(0)
+#             else:
+#                 print(-1)
+# elif z >= 0:
+if s < 0:
+    z -= 1
+    s += 23
+if d < 0:
+    s -= 1
+    d += 59
+answer = z*24*60 + s*60 + d
+if answer < 0:
+    print(-1)
 else:
-    if s < 0:
-        z -= 1
-        s += 23
-    if d < 0:
-        s -= 1
-        d += 59
-    print(z*24*60 + s*60 + d)
+    print(answer)
