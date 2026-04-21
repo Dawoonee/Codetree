26f6b3846d3a70d2435bad9cd928cb4bed82cb14
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-21 22:34:41 +0900

    Title: 그 요일은, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,16 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+def sum_d(m, d):
+    return sum(arr[:m]) + d
+a = sum_d(m1, d1)
+b = sum_d(m2, d2)
+diff = b - a
+# print(diff)
+answer = (diff + 1)//7
+if (diff + 1)%7 == t.index(A)-1:
+    answer += 1
+print(answer)
