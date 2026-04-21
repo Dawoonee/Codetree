e9e8a119f0e935190b2bbf16cc3c32a1652cb522
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-21 22:23:35 +0900

    Title: 요일 맞추기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/요일 맞추기/guess-day-of-week.py
+++ 260421/요일 맞추기/guess-day-of-week.py
@@ -1,27 +1,12 @@
-def check (m1, d1, m2, d2):
-    a, b = m1*100 + d1, m2*100 + d2
-    if a <= b:
-        return True
-    else:
-        return False
+
 m1, d1, m2, d2 = map(int, input().split())
 arr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 t = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 #True 오른쪽 False 왼쪽
-if check(m1, d1, m2, d2):
-    step = 0
-    for i in range(m1+1, m2):
-        step += arr[i]
-    step += d2
-    step = step - d1
-    step = (1 + step)%7
-    print(t[step])
-
-else:
-    step = 0
-    for i in range(m2+1, m1):
-        step += arr[i]
-    step += d1
-    step = step - d2
-    step = (1 - step)%7
-    print(t[step])
+def sum_d(m, d):
+    return sum(arr[:m]) + d
+a = sum_d(m1, d1)
+b = sum_d(m2, d2)
+dif = b - a
+step = (1 + dif)%7
+print(t[step])
