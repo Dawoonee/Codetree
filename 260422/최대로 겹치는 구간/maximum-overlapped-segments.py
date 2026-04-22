02e56bcc83b1989197373769e3b5bd63e96ab834
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-22 16:21:43 +0900

    Title: 최대로 겹치는 구간, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260422/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 260422/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -4,6 +4,6 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 arr = [0]*201
 for a, b in segments:
-    for j in range(a+100, b+101):
+    for j in range(a+100, b+100):
         arr[j] += 1
 print(max(arr))
