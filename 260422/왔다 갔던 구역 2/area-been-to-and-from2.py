de4153527ec749f08409485cc680e61ae5ed23d0
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-22 19:07:41 +0900

    Title: 왔다 갔던 구역 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260422/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 260422/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -12,23 +12,18 @@ for i in range(n):
     # print(arr[989:1003])
     if dir[i] == 'R':
         # arr[start] -= 1
-        for j in range(start, start + x[i]+1):
+        for j in range(start, start + x[i]):
             arr[j] += 1
         start = j
 
     else:
         # arr[start] -= 1
-        for j in range(start , start- x[i]-1 , -1):
+        for j in range(start- x[i], start):
             arr[j] += 1
         start = j
 answer = 0
-cnt = 0
 for z in arr:
     if z >= 2:
-        cnt += 1
-    else:
-        if cnt == 0:
-            continue
-        answer += cnt-1
-        cnt = 0
+        answer += 1
+
 print(answer)
