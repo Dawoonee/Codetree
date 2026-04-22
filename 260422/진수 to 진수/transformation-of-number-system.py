530c5cbc8e6b3bbf85446642e032846716733052
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-22 15:59:55 +0900

    Title: 진수 to 진수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260422/진수 to 진수/transformation-of-number-system.py
+++ 260422/진수 to 진수/transformation-of-number-system.py
@@ -1,15 +1,13 @@
 a, b = map(int, input().split())
 n = input()
 N = int(n, a)
-arr = []
-i  = 0
-while b**i <= N:
-    i += 1
-while N > 0:
-    if i == 0:
-        break
-    a = N // b**(i-1)
-    arr. append(a)
-    N -= b**(i-1)*a
-    i -= 1
-print(''.join(map(str, arr)))
+
+if N == 0:
+    print('0')
+else:
+    arr = []
+    while N > 0:
+        r = N % b
+        arr.append(str(r))
+        N //= b
+    print(''.join(arr[::-1]))
