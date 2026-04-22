c5e00212b65f033b86f81e30e7de4f8a08a57a23
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-23 00:55:42 +0900

    Title: 신기한 타일 뒤집기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260423/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,36 @@
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+
+arr = [['', 0, 0] for _ in range(200001)]
+start = 100000
+# arr[1000] = 1
+for i in range(n):
+
+    if dir[i] == 'R':
+        # arr[start] -= 1
+        for j in range(start, start + x[i]):
+            arr[j][1] += 1
+            arr[j][0] = 'B'
+        start  += x[i] -1
+
+    else:
+        # arr[start] -= 1
+        for j in range(start- x[i], start):
+            arr[j+1][2] += 1
+            arr[j+1][0] = 'W'
+        start -= x[i] -1
+
+a, b, c = 0, 0, 0
+#흰 , 검 , 회
+for z, b_cnt, w_cnt in arr:
+    if z == 'B':
+        b += 1
+    elif z == 'W':
+        a += 1
+
+print(f'{a} {b}')
