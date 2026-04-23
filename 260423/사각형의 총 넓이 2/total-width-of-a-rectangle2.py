93958bf7c46b725c9d2aa1d770056f11bf8d0d3b
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-23 19:03:32 +0900

    Title: 사각형의 총 넓이 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 260423/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -7,12 +7,16 @@ for _ in range(n):
     x2.append(c)
     y2.append(d)
 
-#x = x2 - x1
-#h = y2 - y1
-#첫 번째 그 이후에
-answer = (x2[0] - x1[0]) * (y2[0] - y1[0])
-for i in range(1, n):
-    x = x2[i-1] - x1[i]
-    h = y2[i] - y1[i]
-    answer += (x*h)
-print(answer)
+arr = [[0]*201 for _ in range(201)]
+point = 100
+
+for i in range(n):
+    for y in range(point + y1[i], point + y2[i]):
+        for x in range(point + x1[i], point + x2[i]):
+            arr[y][x] += 1
+cnt = 0
+for y in range(201):
+    for x in range(201):
+        if arr[y][x] >= 1:
+            cnt += 1
+print(cnt)
