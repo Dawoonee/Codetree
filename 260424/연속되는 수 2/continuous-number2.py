64349637194109d28d9329c65d61d12fb675adbf
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-24 00:36:48 +0900

    Title: 연속되는 수 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/연속되는 수 2/continuous-number2.py
+++ 260424/연속되는 수 2/continuous-number2.py
@@ -1,9 +1,9 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 max_cnt = 0
-cnt = 1
-for i in range(1, n):
-    if arr[i-1] == arr[i]:
+cnt = 0
+for i in range(n):
+    if cnt == 0 or arr[i-1] == arr[i]:
         cnt += 1
     elif arr[i-1] != arr[i]:
         cnt = 1
