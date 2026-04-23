e2186909a75b5a957e70b3971939dbbe62137be3
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-24 01:01:05 +0900

    Title: 연속되는 수 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/연속되는 수 3/continuous-number3.py
+++ 260424/연속되는 수 3/continuous-number3.py
@@ -5,9 +5,9 @@ max_cnt = 0
 cnt = 0
 
 for i in range(N):
-    if cnt == 0 or abs(arr[i-1] + arr[i]) > abs(arr[i]):
+    if cnt == 0 or arr[i-1]*arr[i] > 0:
         cnt += 1
-    elif abs(arr[i-1] + arr[i]) < abs(arr[i]):
+    elif arr[i-1]*arr[i] < 0:
         cnt = 1
     max_cnt = max(max_cnt, cnt)
 print(max_cnt)
