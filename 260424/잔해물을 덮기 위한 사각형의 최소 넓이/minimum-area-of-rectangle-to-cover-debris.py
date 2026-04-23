b16aed5dc7ed77916bb1999c985fc41f100cccd9
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-24 00:08:17 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 0ms, Memory: 0MB, Status: Memory Limit Exceed - Codetree

--- 260424/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 260424/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -8,8 +8,8 @@ point = 1000
 for y in range(point + y1[0], point + y2[0]+1):
     for x in range(point + x1[0], point + x2[0]+1):
         arr[y][x] += 1
-for y in range(point + y1[1], point + y2[1]+1):
-    for x in range(point + x1[1], point + x2[1]+1):
+for y in range(point + y1[1]+1, point + y2[1]):
+    for x in range(point + x1[1]+1, point + x2[1]):
         arr[y][x] -= 1
 
 cnt = []
@@ -18,7 +18,7 @@ for y in range(2001):
         if arr[y][x] == 1:
             cnt.append((x, y))
 a, b, c, d = 0, 2001, 0, 2001
-# print(cnt)
+
 if len(cnt) == 0:
     print(0)
 else:
@@ -27,6 +27,6 @@ else:
         b = min(b, x)
         c = max(c, y)
         d = min(d, y)
-    # print(a, b, c, d)
+
     answer = (a-b)*(c-d)
     print(answer)
