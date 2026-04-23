02ec4561b43148941a62103a83003476a0d4792a
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-24 01:09:27 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260424/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -0,0 +1,13 @@
+n, t = map(int, input().split())
+arr = list(map(int, input().split()))
+
+max_cnt = 0
+cnt = 0
+
+for i in range(n):
+    if t < arr[i]:
+        cnt += 1
+    else:
+        cnt = 0
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
