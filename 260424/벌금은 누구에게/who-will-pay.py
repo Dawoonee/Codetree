79a15a98825b2a6a7340e6eb6f61ccb9945a5427
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-24 19:49:06 +0900

    Title: 벌금은 누구에게, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/벌금은 누구에게/who-will-pay.py
+++ 260424/벌금은 누구에게/who-will-pay.py
@@ -3,12 +3,14 @@ student = [int(input()) for _ in range(M)]
 arr = [0]*(N+1)
 for i in student:
     arr[i] += 1
+    if arr[i] == K:
+        print(i)
+        break
 answer = 0
 for i in range(N):
     if arr[i] >= K:
         answer = i
         break
+
 if answer == 0:
     print(-1)
-else:
-    print(answer)
