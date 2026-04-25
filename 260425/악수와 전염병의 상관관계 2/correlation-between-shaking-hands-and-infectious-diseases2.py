e96de3876da85dca9036b83c267c9b9196867981
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-25 10:15:23 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260425/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 260425/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -3,11 +3,20 @@ handshakes = [tuple(map(int, input().split())) for _ in range(T)]
 
 handshakes = sorted(handshakes, key= lambda x: x[0])
 
-infection = [0]*(N+1)
-infection[P] = 1
+infection = [[0,P] for _ in range(N+1)]
+infection[P][0] = 1
 
 for t, x ,y in handshakes:
-    if (x == P or y == P) and K >0:
-        infection[x], infection[y] = 1, 1
-        K -= 1
-print(''.join(map(str, infection[1:])))
+    if infection[x][0] == 1 and infection[x][1] > 0:
+        infection[y][0] = 1
+        infection[x][1] -= 1
+    elif infection[y][0] == 1 and infection[y][1] > 0:
+        infection[x][0] = 1
+        infection[y][1] -= 1
+    elif infection[x][0] == 1 and infection[y][0] == 1:
+        if infection[x][1] > 0:
+            infection[x][1] -= 1
+        if infection[y][1] > 0:
+            infection[y][1] -= 1
+
+print(''.join(map(str, [infection[i][0] for i in range(1, N+1)])))
