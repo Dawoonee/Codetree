7c537741efc2ad22ea1c05b8109ab2374ee47115
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-25 10:18:47 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 260425/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -3,20 +3,21 @@ handshakes = [tuple(map(int, input().split())) for _ in range(T)]
 
 handshakes = sorted(handshakes, key= lambda x: x[0])
 
-infection = [[0,P] for _ in range(N+1)]
+infection = [[0,K] for _ in range(N+1)]
 infection[P][0] = 1
 
 for t, x ,y in handshakes:
-    if infection[x][0] == 1 and infection[x][1] > 0:
+    if infection[x][0] == 1 and infection[y][0] == 1:
+        if infection[x][1] > 0:
+            infection[x][1] -= 1
+        if infection[y][1] > 0:
+            infection[y][1] -= 1
+    elif infection[x][0] == 1 and infection[x][1] > 0:
         infection[y][0] = 1
         infection[x][1] -= 1
     elif infection[y][0] == 1 and infection[y][1] > 0:
         infection[x][0] = 1
         infection[y][1] -= 1
-    elif infection[x][0] == 1 and infection[y][0] == 1:
-        if infection[x][1] > 0:
-            infection[x][1] -= 1
-        if infection[y][1] > 0:
-            infection[y][1] -= 1
+
 
 print(''.join(map(str, [infection[i][0] for i in range(1, N+1)])))
