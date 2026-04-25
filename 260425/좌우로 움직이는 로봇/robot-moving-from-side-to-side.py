df8f72dc93e6941abbebd3315d864fbf87936df9
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-25 09:16:18 +0900

    Title: 좌우로 움직이는 로봇, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260425/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,53 @@
+n, m = map(int, input().split())
+
+# Process robot A's movements
+t = []
+d = []
+for _ in range(n):
+    time, direction = input().split()
+    t.append(int(time))
+    d.append(direction)
+
+# Process robot B's movements
+t_b = []
+d_b = []
+for _ in range(m):
+    time, direction = input().split()
+    t_b.append(int(time))
+    d_b.append(direction)
+
+a = [0]*2000001
+pos_a = 0
+time_a = 1
+for i in range(n):
+    if d[i] == 'R':
+        for x in range(1, t[i]+1):
+            a[time_a] = pos_a+x
+            time_a += 1
+        pos_a += t[i]
+    elif d[i] == 'L':
+        for x in range(1, t[i]+1):
+            a[time_a] = pos_a-x
+            time_a += 1
+        pos_a -= t[i]
+
+b= [0]*2000001
+pos_b = 0
+time_b = 1
+for i in range(m):
+    if d_b[i] == 'R':
+        for x in range(1, t_b[i]+1):
+            b[time_b] = pos_b+x
+            time_b += 1
+        pos_b += t_b[i]
+    elif d_b[i] == 'L':
+        for x in range(1, t_b[i]+1):
+            b[time_b] = pos_b-x
+            time_b += 1
+        pos_b -= t_b[i]
+cnt = 0
+
+for i in range(1, 2000001):
+    if a[i-1] != b[i-1] and a[i] == b[i]:
+        cnt += 1
+print(cnt)
