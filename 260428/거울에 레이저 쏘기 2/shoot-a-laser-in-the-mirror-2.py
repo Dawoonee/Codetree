6433e6047523aff54f707eb2d3214a35027401a8
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-28 09:33:24 +0900

    Title: 거울에 레이저 쏘기 2, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260428/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 260428/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -15,15 +15,12 @@ for i in range(k):
         d += 1
         if d > 3:
             d = 0
-visited = [[False]*(n+2) for _ in range(n+2)]
 for i in range(4):
     nx = x + dx[i]
     ny = y + dy[i]
     if grid[ny][nx] == '.':
-        visited[ny][nx] = True
         break
 d = i
-
 d += 1
 if d >3:
     d -= 3
@@ -31,7 +28,6 @@ cnt = 0
 while True:
     if grid[y][x] == '.':
         break
-    visited[y][x] = True
     if grid[y][x] == '\\':
         d = d^1
     elif grid[y][x] == '/':
