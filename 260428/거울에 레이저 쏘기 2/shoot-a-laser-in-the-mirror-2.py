c7160c645180ec0e8a61656c80b61f8de86d171c
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-28 13:23:00 +0900

    Title: 거울에 레이저 쏘기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 260428/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,6 +1,6 @@
 n = int(input())
 grid = [list('.' + input().strip() + '.') for _ in range(n)]
-k = int(input()) - 1
+k = int(input())-1
 grid = [['.'] * (n + 2)] + grid + [['.'] * (n + 2)]
 
 dx = [1, 0, -1, 0]
@@ -8,35 +8,27 @@ dy = [0, 1, 0, -1]
 
 x, y = 1, 1
 d = 0
-for _ in range(k):
+for i in range(k):
     nx = x + dx[d]
     ny = y + dy[d]
+    if nx == 1 and ny == 1:
+        break
     if 1 <= nx <= n and 1 <= ny <= n:
         x, y = nx, ny
     else:
-        d = (d + 1) % 4
-        x += dx[d]
-        y += dy[d]
-
-for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-    if grid[ny][nx] == '.':
-        d = (i + 2) % 4
-        break
-
+        d = (d+1)%4
+d = (d+1)%4
 cnt = 0
 while True:
-    if grid[y][x] == '.':
+    if grid[y][x] =='.':
         break
 
     if grid[y][x] == '\\':
-        d = d ^ 1
+        d = d^1
     elif grid[y][x] == '/':
-        d = d ^ 3
+        d = d^3
 
     x += dx[d]
     y += dy[d]
     cnt += 1
-
 print(cnt)
