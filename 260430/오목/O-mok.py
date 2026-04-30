4d32d16d0bc03cdfa8a11553e9b2289ed812135f
Dawoonee <132527227+Dawoonee@users.noreply.github.com>
2026-04-30 13:04:56 +0900

    Title: 오목, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/오목/O-mok.py
@@ -0,0 +1,109 @@
+board = [list(map(int, input().split())) for _ in range(19)]
+#오른쪽
+for i in range(19):
+    cnt_b, cnt_w = 0, 0
+    for j in range(19):
+        if board[i][j] == 0:
+            cnt_b = 0
+            cnt_w = 0
+        if board[i][j] == 1:
+            cnt_b += 1
+            cnt_w = 0
+        if board[i][j] == 2:
+            cnt_b = 0
+            cnt_w += 1
+        if cnt_b == 5:
+            print(1)
+            print(i+1, j-1)
+            break
+        if cnt_w == 5:
+            print(2)
+            print(i+1, j-1)
+            break
+#아래
+for i in range(19):
+    cnt_b, cnt_w = 0, 0
+    for j in range(19):
+        if board[j][i] == 0:
+            cnt_b = 0
+            cnt_w = 0
+        if board[j][i] == 1:
+            cnt_b += 1
+            cnt_w = 0
+        if board[j][i] == 2:
+            cnt_b = 0
+            cnt_w += 1
+        if cnt_b == 5:
+            print(1)
+            print(i-1, j+1)
+            break
+        if cnt_w == 5:
+            print(2)
+            print(i-1, j+1)
+            break
+# 대각선 오른쪽
+for i in range(19):
+    cnt_b, cnt_w = 0, 0
+    for j in range(19):
+        x, y = i + j, j
+        if x >= 19 or y >= 19:
+            continue
+        if board[y][x] == 0:
+            cnt_b = 0
+            cnt_w = 0
+        if board[y][x] == 1:
+            cnt_b += 1
+            cnt_w = 0
+        if board[y][x] == 2:
+            cnt_b = 0
+            cnt_w += 1
+        if board[x][y] == 0:
+            cnt_b = 0
+            cnt_w = 0
+        if board[x][y] == 1:
+            cnt_b += 1
+            cnt_w = 0
+        if board[x][y] == 2:
+            cnt_b = 0
+            cnt_w += 1
+        if cnt_b == 5:
+            print(1)
+            print(i-1, j-1)
+            break
+        if cnt_w == 5:
+            print(2)
+            print(i-1, j-1)
+            break
+#대각선 왼쪽
+for i in range(19):
+    cnt_b, cnt_w = 0, 0
+    for j in range(19):
+        x, y = j , i+j
+        if x >= 19 or y >= 19:
+            continue
+        if board[y][x] == 0:
+            cnt_b = 0
+            cnt_w = 0
+        if board[y][x] == 1:
+            cnt_b += 1
+            cnt_w = 0
+        if board[y][x] == 2:
+            cnt_b = 0
+            cnt_w += 1
+        if board[x][y] == 0:
+            cnt_b = 0
+            cnt_w = 0
+        if board[x][y] == 1:
+            cnt_b += 1
+            cnt_w = 0
+        if board[x][y] == 2:
+            cnt_b = 0
+            cnt_w += 1
+        if cnt_b == 5:
+            print(1)
+            print(i+1, j+1)
+            break
+        if cnt_w == 5:
+            print(2)
+            print(i+1, j+1)
+            break
